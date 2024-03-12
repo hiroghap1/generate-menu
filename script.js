4c0ab7ebf9a43c2a9f855a5519070f528f66884c
hiroghap1 <hiro_ghap1@yahoo.co.jp>
2024-03-12 18:41:01 +0900

    feat(index):vuedraggable追加

--- script.js
+++ script.js
@@ -1,88 +1,23 @@
 Vue.createApp({
     setup() {
-        const result = Vue.ref('');
-        const searchTitle = Vue.ref('');
-        const searchOnly = Vue.ref(false);
-        const searchResultURL = Vue.ref('');
-        const setCategory = Vue.ref('');
-        const setTag = Vue.ref('');
-        const addSubCategories = Vue.ref([]);
-        const tabGroups = Vue.ref([{ name: '', tabs: [{ name: '', imageURL: '' }] }]);
+        const menuList = Vue.ref( [
+            { name: 'Home', url: 'https://example.com/home', isExternal: false },
+            { name: 'About', url: 'https://example.com/about', isExternal: false }
+        ]);
 
-        const menuList = Vue.ref([{name: '',url: '', blank: false}]);
 
-        function addTabGroup() {
-            this.tabGroups.push({ name: '', tabs: [{ name: '', imageURL: '' }] });
-        }
-        function removeTabGroup(index) {
-            if (this.tabGroups.length > 1) {
-                this.tabGroups.splice(index, 1);
-            } else {
-                alert('これ以上削除できません');
-            }
-        }
-        function addSubCategory() {
-            this.addSubCategories.push('');
-            // this.$set(this.addSubCategories,'');
-        }
-        function removeSubCategory(categoryIndex) {
-            if (this.addSubCategories.length > 0) {
-                this.addSubCategories.splice(categoryIndex, 1);
-            }
-        }
-        function addTab(tabGroupIndex) {
-            console.log('addTab called');
-            if (!this.tabGroups[tabGroupIndex].tabs) {
-                console.log('tabs property does not exist, creating it');
-                this.$set(this.tabGroups[tabGroupIndex], 'tabs', []);
-            }
-            this.tabGroups[tabGroupIndex].tabs.push({ name: '', imageURL: '' });
-            console.log('tabGroups:', this.tabGroups);
-        }
-        function removeTab(tabGroupIndex, tabIndex) {
-            if (this.tabGroups[tabGroupIndex].tabs.length > 1) {
-                this.tabGroups[tabGroupIndex].tabs.splice(tabIndex, 1);
-            } else {
-                alert('これ以上削除できません');
-            }
-        }
         function fileGenerate () {
-            const searchSettings = {
-                searchTitle: searchTitle.value,
-                searchOnly: searchOnly.value,
-                searchResultURL: searchResultURL.value,
-                setCategory: setCategory.value,
-                setTag: setTag.value,
-                addSubCategory: addSubCategories.value,
-                tags: {
-                },
+            const menu = {
             };
-            tabGroups.value.forEach((group) => {
-                searchSettings.tags[group.name] = group.tabs.map(tab => ({ name: tab.name, image: tab.imageURL }));
-            });
 
-            result.value = `<script>
-  // こだわり検索セッティング
-  const searchSettings = ${JSON.stringify(searchSettings, null, 2)};
-</script>`;
+            result.value = ``;
         }
         return {
-            tabGroups,
-            result,
-            searchTitle,
-            searchOnly,
-            searchResultURL,
-            setCategory,
-            setTag,
-            addSubCategories,
-            addTabGroup,
-            removeTabGroup,
-            addSubCategory,
-            removeSubCategory,
-            addTab,
-            removeTab,
             fileGenerate,
-            menuList
+            menuList,
+            component: {
+                draggable: window['vuedraggable']
+            }
         };
     }
 }).mount('#app');
